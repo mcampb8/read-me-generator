e18bcef1b1e095f535ed98dd5f2684a0aac6e02f
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-04-22 14:52:45 -0700

    final updates

--- index.js
+++ index.js
@@ -69,13 +69,13 @@ inquirer
     .then((data) => {
         const readMeStr =
             `# ${data.title}
-
+            
+## Badges
+${renderLicenseBadge(data.license)}
 ## Description
 ${data.desc}
 
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
@@ -83,48 +83,20 @@ If your README is long, add a table of contents to make it easy for users to fin
 - [License](#license)
 
 ## Installation
-
-These are the steps to Install my Project:
 ${data.install}
 
 ## Usage
-
-These are the instructions for how to use this product:
 ${data.usage}
 
-To add a screenshot, create an "assets/images" folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    md
-    ![alt text](assets/images/screenshot.png)
-    
-
 ## Credits
-
-I collaborated with: 
 ${data.contributors}
 
 ## License
 ${returnLicense(data.license)}
 ---
 
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-${renderLicenseBadge(data.license)}
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
 ## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+${data.tests}
 ## Questions
 [Email](mailto:${data.email})
 [Github](https://www.github.com/${data.github})
---
+++ project description/README-1.md
@@ -0,0 +1,33 @@
+# Read Me Generator
+            
+## Badges
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description
+This Application can take in user input and Generate a ReadME file based on the data received from the user. 
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+There are no installation instructions for this program
+
+## Usage
+This project will be used to help generate a ReadME file for other projects
+
+## Credits
+I worked on this project and received some tutoring from David Elutilo at BootcampSpot
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+---
+## Video Walkthrough
+[Video](https://drive.google.com/file/d/1byof2o19scSDxmtr7rm9sVd43Vcf14-J/view)
+## Tests
+There are no tests to run for this program
+## Questions
+[Email](mailto:michaelicampbell8@gmail.com)
+[Github](https://www.github.com/mcampb8)
